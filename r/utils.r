e70565eb2a6531897d76a09467a3dc0380b125db
ddhaese <dhaesedavid@gmail.com>
2020-10-20 23:05:48 +0200

    H9

--- r/utils.r
+++ r/utils.r
@@ -5,3 +5,41 @@
     .Primitive("+")(x,y)
   }
 }
+
+
+# Description:
+#  In density plots, the densities sometimes extents outside the domain of a 
+#  variable. This functions takes the densities that fall outside the domain,
+#  'mirrors' them across the respective boundary and sums the reflected portions
+#  of the densities with the density values that fall already within the domain.
+# Usage:
+#   Percentages <- runif ( 500, 0, 1 )
+#   Percentage %>% density %>% Mirror ( 0:1 ) %>% plot
+mirror <- function ( iData, iCuts ){
+  X <- iData$x
+  Y <- iData$y
+  N <- length ( X )
+  
+  H <- which ( X < min ( iCuts ))
+  Nh <- length ( H )
+  
+  if ( Nh > 0 ){
+    Y [( 2 * Nh ):( Nh + 1 )] <- Y [( 2 * Nh ):( Nh + 1 )] +
+      Y [ H ]
+    Y [ H ] <- 0
+  }
+  
+  TT <- which ( X > max ( iCuts ))
+  Nt <- length ( TT )
+  
+  if ( Nt > 0 ){
+    Y [( N - Nt ):( N - 2*Nt + 1)] <- Y [( N - Nt ):( N - 2*Nt + 1)] +
+      Y [ TT ]
+    Y [ TT ] <- 0
+  }
+  
+  iData$x <- X
+  iData$y <- Y
+  
+  return ( iData )
+}
